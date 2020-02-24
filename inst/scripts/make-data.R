930bee6f35a0e20131d953a2e2cbf20ec0f925c2
FelixErnst <felix.gm.ernst@outlook.com>
2020-02-24 01:16:58 +0100

    Update make-data.R

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -14,13 +14,13 @@ seqlevelsStyle(edb) <- "UCSC"
 
 # get transcript annotations from ensemble db
 assemble_tx <- function(edb, genome){
-  # get exons
-  tx <- exonsBy(edb,"tx")
-  tx_id <- IRanges::CharacterList(Map(rep,names(tx),lengths(tx)))
-  mcols(tx, level="within")[,"tx_id"] <- tx_id
-  genome(tx) <- genome
-  #
-  tx[lengths(tx) != 0L]
+    # get exons
+    tx <- exonsBy(edb,"tx")
+    tx_id <- IRanges::CharacterList(Map(rep,names(tx),lengths(tx)))
+    mcols(tx, level="within")[,"tx_id"] <- tx_id
+    genome(tx) <- genome
+    #
+    tx[lengths(tx) != 0L]
 }
 
 tx <- assemble_tx(edb, "sacCer3")
@@ -29,39 +29,43 @@ tx <- assemble_tx(edb, "sacCer3")
 # functions for import
 ################################################################################
 
-import.RMBase <- function(bs, tx, organism, genome, type){
-  browser()
-  seq <- getSeq(bs,tx)
-  seq <- relist(unlist(unlist(seq)),
-                IRanges::PartitioningByWidth(sum(nchar(seq))))
-  seq_rna <- as(seq,"RNAStringSet")
-  #
-  makeEpiTxDbfromRMBase(organism, genome, type, tx, seq_rna)
+import.RMBase <- function(bs, organism, genome, type){
+    metadata <- data.frame(name = c("Genome","Coordinates"),
+                           value = c("sacCer3","per Genome"))
+    #
+    seq <- getSeq(bs)
+    seq_rna <- as(seq,"RNAStringSet")
+    #
+    makeEpiTxDbFromRMBase(organism, genome, type, sequences = seq_rna,
+                          metadata = metadata)
 }
 
 import_from_tRNAdb <- function(bs, tx){
-  seq <- getSeq(bs,tx)
-  seq <- relist(unlist(unlist(seq)),
-                IRanges::PartitioningByWidth(sum(nchar(seq))))
-  seq_rna <- as(seq,"RNAStringSet")
-  makeEpiTxDbFromtRNAdb("Saccharomyces cerevisiae", tx, seq_rna)
+    metadata <- data.frame(name = c("Genome","Coordinates"),
+                           value = c("sacCer3","per Genome"))
+    #
+    seq <- getSeq(bs,tx)
+    seq <- relist(unlist(unlist(seq)),
+                  IRanges::PartitioningByWidth(sum(nchar(seq))))
+    seq_rna <- as(seq,"RNAStringSet")
+    makeEpiTxDbFromtRNAdb("Saccharomyces cerevisiae", seq_rna,
+                          metadata = metadata)
 }
 
 start.import <- function(bs, tx){
-  etdb <- import.RMBase(bs, tx, "yeast",
-                        "sacCer3",
-                        listAvailableModFromRMBase("yeast", "sacCer3"))
-  db <- dbConnect(SQLite(), "hub/EpiTxDb.Sc.sacCer3.RMBase.sqlite")
-  sqliteCopyDatabase(etdb$conn, db)
-  dbDisconnect(etdb$conn)
-  dbDisconnect(db)
-
-  etdb <- import_from_tRNAdb(bs, tx)
-  db <- dbConnect(SQLite(), "hub/EpiTxDb.Sc.sacCer3.tRNAdb.sqlite")
-  sqliteCopyDatabase(etdb$conn, db)
-  dbDisconnect(etdb$conn)
-  dbDisconnect(db)
-  return(TRUE)
+    etdb <- import.RMBase(bs, "yeast", "sacCer3",
+                          listAvailableModFromRMBase("yeast", "sacCer3"))
+    db <- dbConnect(SQLite(), "hub/EpiTxDb.Sc.sacCer3.RMBase.sqlite")
+    sqliteCopyDatabase(etdb$conn, db)
+    dbDisconnect(etdb$conn)
+    dbDisconnect(db)
+  
+    etdb <- import_from_tRNAdb(bs, tx)
+    db <- dbConnect(SQLite(), "hub/EpiTxDb.Sc.sacCer3.tRNAdb.sqlite")
+    sqliteCopyDatabase(etdb$conn, db)
+    dbDisconnect(etdb$conn)
+    dbDisconnect(db)
+    return(TRUE)
 }
 
 start.import(BSgenome.Scerevisiae.UCSC.sacCer3, tx)
