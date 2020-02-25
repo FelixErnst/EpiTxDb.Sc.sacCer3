ed7ada1223e7aabd967ca04569e93238c6baf699
FelixErnst <felix.gm.ernst@outlook.com>
2020-02-25 20:33:54 +0100

    updated metadata

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -27,7 +27,7 @@ df <- rbind(
         DataFrame(Title = "EpiTxDb tRNAdb for Saccharomyces cerevisiae sacCer3", 
                   Description = paste0(
                     ""),
-                  SourceType = "TXT",
+                  SourceType = "XML",
                   SourceUrl = tRNAdbURL,
                   DataProvider = "tRNAdb",
                   RDataClass = "SQLiteFile", 
